cbf3032e87e24aab8902547c077bbde333bdf2ef
nbhaohao <nbhaohao@gmail.com>
2026-07-04 14:45:48 +0800

    0104

--- 0104/log.go
+++ 0104/log.go
@@ -23,7 +23,12 @@ func (log *Log) Close() error {
 //  1. log.fp.Write(ent.Encode())，出错返回
 //  2. return log.fp.Sync()  // Sync() 就是 fsync 系统调用
 //     普通 Write 只到 OS 的 page cache，断电就丢；Sync() 强制刷到硬盘并等待完成 = durability
-func (log *Log) Write(ent *Entry) error
+func (log *Log) Write(ent *Entry) error {
+	if _, err := log.fp.Write(ent.Encode()); err != nil {
+		return err
+	}
+	return log.fp.Sync()
+}
 
 func (log *Log) Read(ent *Entry) (eof bool, err error) {
 	err = ent.Decode(log.fp)
