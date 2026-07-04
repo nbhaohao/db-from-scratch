c759974c839f54fd9bea19eccf22d5abe3184419
nbhaohao <nbhaohao@gmail.com>
2026-07-04 20:27:50 +0800

    godb 0302: parseValue/parseInt/parseString (with escaping)

--- 0302/sql_parser.go
+++ 0302/sql_parser.go
@@ -2,6 +2,7 @@ package db0302
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 )
 
@@ -90,14 +91,59 @@ func (p *Parser) parseValue(out *Cell) error {
 //  3. 遇到和开头相同的 quote：字符串结束，out.Type = TypeStr，p.pos = cur+1（跳过收尾引号），return nil
 //  4. 其他字符：append 进 out.Str，cur++
 //  5. 扫到 buf 末尾还没遇到收尾引号：return errors.New("string is not terminated")
-func (p *Parser) parseString(out *Cell) error
+func (p *Parser) parseString(out *Cell) error {
+	quote := p.buf[p.pos]
+	cur := p.pos + 1
+	for cur < len(p.buf) {
+		ch := p.buf[cur]
+		if ch == '\\' {
+			if cur+1 >= len(p.buf) {
+				return errors.New("bad escape")
+			}
+			next := p.buf[cur+1]
+			if next != quote && next != '\\' {
+				return errors.New("bad escape")
+			}
+			out.Str = append(out.Str, next)
+			cur += 2
+		} else if ch == quote {
+			out.Type = TypeStr
+			p.pos = cur + 1
+			return nil
+		} else {
+			out.Str = append(out.Str, ch)
+		}
+		cur++
+	}
+	return errors.New("string is not terminated")
+}
 
 // 你来实现（解析一个有符号整数字面量）：
 //  1. start, cur := p.pos, p.pos；开头若是 '-'/'+' 先 cur++ 跳过符号
 //  2. 用 isDigit 继续往前扫数字
 //  3. strconv.ParseInt(p.buf[start:cur], 10, 64) 解析出 out.I64，失败直接 return err
 //  4. 成功：out.Type = TypeI64，p.pos = cur，return nil
-func (p *Parser) parseInt(out *Cell) (err error)
+func (p *Parser) parseInt(out *Cell) (err error) {
+	start := p.pos
+	cur := p.pos
+	if cur < len(p.buf) && (p.buf[cur] == '-' || p.buf[cur] == '+') {
+		cur++
+	}
+	for cur < len(p.buf) && isDigit(p.buf[cur]) {
+		cur++
+	}
+	if cur == start {
+		return errors.New("invalid integer")
+	}
+	i64, err := strconv.ParseInt(p.buf[start:cur], 10, 64)
+	if err != nil {
+		return err
+	}
+	out.Type = TypeI64
+	out.I64 = i64
+	p.pos = cur
+	return nil
+}
 
 func (p *Parser) isEnd() bool {
 	p.skipSpaces()
